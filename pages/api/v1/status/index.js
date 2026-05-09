05b7acc61036ac0d1c4606cd65a57ce49b40e651
IHAS <ismaelhenriqueeu@gmail.com>
2026-05-09 16:19:25 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,47 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
+
+
+
+  const updateAt = new Date().toISOString();
+
+  const versionResult = await database.query('SHOW server_version;');
+
+  const maxConnResult = await database.query('SHOW max_connections');
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  
+  const openConnResult = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  // const openConnResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db';");
+
+  const dbVersion = versionResult.rows[0].server_version;
+
+  const maxConn = maxConnResult.rows[0].max_connections;
+
+  const openConn = openConnResult.rows[0].count;
+
+  console.log(openConn);
+
+
+
   response.status(200).json({
-    "chave": "são acima da média"
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(maxConn),
+        current_connections: parseInt(openConn),
+      }
+    },
+  
+
   });
 }
 
 export default status;
+
