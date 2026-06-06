201ddc4ddcc63a311db750525e7cce6322ce9c82
IHAS <ismaelhenriqueeu@gmail.com>
2026-06-06 09:50:13 -0300

    feat: create and use `InternalServerError` in `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,38 +1,50 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
+  try {
+    const updateAt = new Date().toISOString();
 
-  const versionResult = await database.query("SHOW server_version;");
+    const versionResult = await database.query("SHOW server_version;");
 
-  const maxConnResult = await database.query("SHOW max_connections");
+    const maxConnResult = await database.query("SHOW max_connections");
 
-  const databaseName = process.env.POSTGRES_DB;
+    const databaseName = process.env.POSTGRES_DB;
 
-  const openConnResult = await database.query({
-    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  // const openConnResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db';");
+    const openConnResult = await database.query({
+      text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    // const openConnResult = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db';");
 
-  const dbVersion = versionResult.rows[0].server_version;
+    const dbVersion = versionResult.rows[0].server_version;
 
-  const maxConn = maxConnResult.rows[0].max_connections;
+    const maxConn = maxConnResult.rows[0].max_connections;
 
-  const openConn = openConnResult.rows[0].count;
+    const openConn = openConnResult.rows[0].count;
 
-  console.log(openConn);
+    console.log(openConn);
 
-  response.status(200).json({
-    updated_at: updateAt,
-    dependencies: {
-      database: {
-        version: dbVersion,
-        max_connections: parseInt(maxConn),
-        current_connections: parseInt(openConn),
+    response.status(200).json({
+      updated_at: updateAt,
+      dependencies: {
+        database: {
+          version: dbVersion,
+          max_connections: parseInt(maxConn),
+          current_connections: parseInt(openConn),
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controler:");
+    console.error(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
