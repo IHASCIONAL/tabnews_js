6fffe7291b8c38884ff9897fef0d71c0ae9efe78
IHAS <ismaelhenriqueeu@gmail.com>
2026-06-28 11:07:41 -0300

    feat: consider `resource` in `authorization` model

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -57,8 +57,6 @@ describe("Default user", () => {
     expect(response.status).toBe(404);
   });
   test("With duplicated username", async () => {
-    //const createdUser = await orchestrator.createUser();
-
     await orchestrator.createUser({
       username: "user1",
     });
@@ -90,6 +88,42 @@ describe("Default user", () => {
       message: "O username informado já está sendo utilizado",
       name: "ValidationError",
       status_code: 400,
+    });
+  });
+
+  test("With `UserB` targeting `userA`", async () => {
+    await orchestrator.createUser({
+      username: "userA",
+    });
+
+    const createdUserB = await orchestrator.createUser({
+      username: "UserB",
+    });
+
+    const activatedUserB = await orchestrator.activateUser(createdUserB);
+    const sessionObject2 = await orchestrator.createSession(activatedUserB.id);
+
+    const response = await fetch("http://localhost:3000/api/v1/users/userA", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `session_id=${sessionObject2.token}`,
+      },
+      body: JSON.stringify({
+        username: "userC",
+      }),
+    });
+
+    expect(response.status).toBe(403);
+
+    const responseBody = await response.json();
+
+    expect(responseBody).toEqual({
+      action:
+        "Verifique se você possui a feature necessária para atualizar outro usuário",
+      message: "Você não possui permissão para atualizar outro usuário.",
+      name: "ForbiddenError",
+      status_code: 403,
     });
   });
 
